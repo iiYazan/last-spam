7b103243a2a1e8fc79dcb5ba1ab994fa79408439
iiYazan <45070056+iiYazan@users.noreply.github.com>
2018-11-17 02:57:56 -0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,34 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "1";
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+client.on('ready', async() => {
+var server = "512988497850662912"; // 
+var channel = "513294978777219073";//
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**yazan_ , yazan_ , yazan_ , yazan_ , yazan_ , yazan_ , yazan_ , yazan_ , yazan_ , yazan_ , yazan_ , yazan_ , yazan_ , yazan_ , yazan_ , yazan_ , yazan_ , yazan_ , **')
+    },305);
+})
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "--") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+ 
+client.login(process.env.BOT_TOKEN);
